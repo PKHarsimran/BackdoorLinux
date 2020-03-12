e7cdb46b53fba04e3c4aa86e1eaf8b4d48cc0ce2
Harsimran sidhu <harsimransidhu770@gmail.com>
2020-03-11 21:03:16 -0400

    Update sessionCreator.sh

--- sessionCreator.sh
+++ sessionCreator.sh
@@ -2,13 +2,13 @@
 echo "Start listening "
 #######################
 #First check for ssh connection
-username=pkvirus
+username=user
 TARGET_IP=192.168.7.141
 #ssh $username@$TARGET_IP -p 22
 #going to try something new here
 ###########################
 sudo apt-get install sshpass -y
-sshpass -p 'Gu5wafre' ssh $username@$TARGET_IP -p 22
+sshpass -p 'password' ssh $username@$TARGET_IP -p 22
 if [ $(whoami) != 'pkvirus' ]; then
     echo "PWNED ! SSH is good "
 
